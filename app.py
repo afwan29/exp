e3afbea87008a8b44dc52c5f7ab7f69703ddde79
afwan29 <136504723+afwan29@users.noreply.github.com>
2026-06-27 03:55:36 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-app = Flask(name)
+app = Flask(__name__)
 
 @app.route("/")
 def home():
-return "Website Running Successfully"
+    return "Website Running Successfully"
 
-if name == "main":
-app.run()
+if __name__ == "__main__":
+    app.run()
