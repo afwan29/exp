d038f6f209ccd0fff778de16b1c8478e02b8dc5c
afwan29 <136504723+afwan29@users.noreply.github.com>
2026-06-27 03:53:15 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,10 @@
-from flask import Flask, render_template, request, redirect
-import sqlite3
+from flask import Flask
 
-app = Flask(**name**)
-
-def db():
-conn = sqlite3.connect("store.db")
-conn.row_factory = sqlite3.Row
-return conn
+app = Flask(name)
 
 @app.route("/")
 def home():
 return "Website Running Successfully"
 
-if **name** == "**main**":
+if name == "main":
 app.run()
